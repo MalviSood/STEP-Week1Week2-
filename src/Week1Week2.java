6d7217eee95b275a8dc2eb96b5434f80d27dc853
MalviSood <malvisood@gmail.com>
2026-03-04 23:09:47 +0530

    P1: Social Media username availability checker

--- src/Week1Week2.java
+++ src/Week1Week2.java
@@ -1,4 +1,69 @@
-public class Week1Week2 {
+import java.util.*;
 
+public class Week1Week2{
+
+    public static void main(String[] args) {
+
+        UsernameChecker checker = new UsernameChecker();
+
+        checker.registerUser("john_doe", 1);
+
+        System.out.println("Is john_doe available? " + checker.checkAvailability("john_doe"));
+        System.out.println("Is jane_smith available? " + checker.checkAvailability("jane_smith"));
+
+        System.out.println("Suggestions: " + checker.suggestAlternatives("john_doe"));
+
+        System.out.println("Most attempted username: " + checker.getMostAttempted());
+    }
 }
 
+class UsernameChecker {
+
+    private Map<String, Integer> usernameMap = new HashMap<>();
+    private Map<String, Integer> attemptFrequency = new HashMap<>();
+
+    public boolean checkAvailability(String username) {
+
+        attemptFrequency.put(username,
+                attemptFrequency.getOrDefault(username, 0) + 1);
+
+        return !usernameMap.containsKey(username);
+    }
+
+    public void registerUser(String username, int userId) {
+        usernameMap.put(username, userId);
+    }
+
+    public List<String> suggestAlternatives(String username) {
+
+        List<String> suggestions = new ArrayList<>();
+
+        for (int i = 1; i <= 3; i++) {
+            String alt = username + i;
+
+            if (!usernameMap.containsKey(alt)) {
+                suggestions.add(alt);
+            }
+        }
+
+        suggestions.add(username.replace("_", "."));
+
+        return suggestions;
+    }
+
+    public String getMostAttempted() {
+
+        String maxUser = null;
+        int max = 0;
+
+        for (Map.Entry<String, Integer> entry : attemptFrequency.entrySet()) {
+
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                maxUser = entry.getKey();
+            }
+        }
+
+        return maxUser;
+    }
+}
